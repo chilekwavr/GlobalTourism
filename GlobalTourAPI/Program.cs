9483372edfd2bd595dd0a6c54c222e10eca0fa37
Victor Chilekwa <Victor.Chilekwa@cqc.org.uk>
2021-07-15 14:22:44 +0100

    azure correct

--- GlobalTourAPI/Program.cs
+++ GlobalTourAPI/Program.cs
@@ -6,7 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Identity;
+//using Azure.Identity;
 
 namespace GlobalTourAPI
 {
@@ -19,14 +19,25 @@ namespace GlobalTourAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        //public static void Main(string[] args)
+        //{
+        //    CreateHostBuilder(args).Build().Run();
+        //}
+
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //    .ConfigureAppConfiguration((context, config) =>
+        //    {
+        //    var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+        //    config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+        //    })
+        //                    .ConfigureWebHostDefaults(webBuilder =>
+        //                    {
+        //                        webBuilder.UseStartup<Startup>();
+        //                    });
+        }
     }
-}
